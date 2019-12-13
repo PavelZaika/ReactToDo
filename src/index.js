7020bfe36851d5038a7ae95fb74cd839e4e04a09
Loki <zpk.loki@gmail.com>
2019-12-13 15:26:09 +0300

    data in array

--- src/index.js
+++ src/index.js
@@ -5,11 +5,18 @@ import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
 
 const App = () => {
+  const todoData = [
+    {label: 'Drink Coffee', important: false},
+    {label: 'Build React App', important: true},
+    {label: 'Done', important: false}
+  ];
+
+
   return (
     <div>
       <AppHeader />
       <SearchPanel />
-      <TodoList />
+      <TodoList todos={todoData}/>
     </div>
   );
 };
