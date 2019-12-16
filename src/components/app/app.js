53b5b574bd8ad48aea129ad6963bada1e546c48e
Loki <zpk.loki@gmail.com>
2019-12-16 17:23:39 +0300

    setState - add item

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -47,6 +47,16 @@ export default class App extends Component {
     
   };
 
+  onToggleImportant = (id) => {
+    console.log("important", id)
+  };
+
+  onToggleDone = (id) => {
+    console.log('done', id)
+  };
+
+
+
   render() {
     return (
       <div className="todo-app">
@@ -56,7 +66,10 @@ export default class App extends Component {
           <ItemStatusFilter />
         </div>
 
-        <TodoList todos={this.state.todoData} onDeleted={this.deleteItem} />
+        <TodoList todos={this.state.todoData} 
+        onDeleted={this.deleteItem} 
+        onToggleImportant = {this.onToggleImportant} 
+        onToggleDone = {this.onToggleDone}/>
 
         <ItemAddForm onItemAdded={this.addItem}/>
       </div>
